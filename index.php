e54768c015f45239d3912097d302a251ec6814d1
Aris Mulyadi <45355305+ariesmo@users.noreply.github.com>
2020-01-17 15:52:12 +0700

    update
    update

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "decodeappserver";
+    $user = "decode";
+    $pass = "D3c0d3123";
+    $db = "decode";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
